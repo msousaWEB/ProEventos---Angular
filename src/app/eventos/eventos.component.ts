48664c2b78d2f86ae69aae7333c51f6940710db1
msousa <matheussousa0241@gmail.com>
2024-08-02 23:17:48 -0300

    layout of grid eventos and filtering data while typing

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -7,12 +7,40 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
+  isCollapsed = false;
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+
+  widthImg = 10;
+  heightImg = 6;
+  marginImg = 0.2;
+
+  private Filtro = '';
+
+  public get filtroLista() {
+    return this.Filtro;
+  }
+
+  public set filtroLista(value: string) {
+    this.Filtro = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   public getEventos(): void {
     this.http.get('https://localhost:44354/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = response;
+      },
       error => console.log(error),
     );
   }
