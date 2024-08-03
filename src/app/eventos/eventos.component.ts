a9b6af10651ed37a3b93ae512e31ee7f7faf6a4e
msousa <matheussousa0241@gmail.com>
2024-08-02 21:03:44 -0300

    first commit for Eventos Web App front end;

---
+++ src/app/eventos/eventos.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  public eventos: any;
+
+  public getEventos(): void {
+    this.http.get('https://localhost:44354/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error),
+    );
+  }
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+}
